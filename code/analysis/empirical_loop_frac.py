36a936cdd011006f45acc003a8a31f8e87f08806
shirokaw <shirokaw@caltech.edu>
2019-06-19 22:30:02 -0700

    Made changes to loops per bead fraction.

--- code/analysis/empirical_loop_frac.py
+++ code/analysis/empirical_loop_frac.py
@@ -10,20 +10,25 @@ import bokeh.plotting
 loop_df = pd.read_csv('../../data/compiled_looping_fraction.csv')
 #%%
 # Create mean looping fraction for each replicate
-loop_df['fraction beads looped'] = loop_df['n_loops'] / loop_df['n_beads']
+loop_df['average_loops_per_bead'] = loop_df['n_loops'] / loop_df['n_beads']
 #%%
 # Compile empirical mean and standard deviation of looping fraction for each mutant
 mutant_names = loop_df['mutant'].unique()
 compiled_loop_df = pd.DataFrame()
 for mutant in mutant_names:
-    mean_fraction = np.mean(loop_df[loop_df['mutant']==mutant]['fraction beads looped'])
-    std_fraction = np.std(loop_df[loop_df['mutant']==mutant]['fraction beads looped'])
+    mean_fraction = np.mean(loop_df[loop_df['mutant']==mutant]['average_loops_per_bead'])
+    std_fraction = np.std(loop_df[loop_df['mutant']==mutant]['average_loops_per_bead'])
+    global_fraction = np.sum(loop_df[loop_df['mutant']==mutant]['n_loops']) / np.sum(loop_df[loop_df['mutant']==mutant]['n_beads'])
     compiled_loop_df = compiled_loop_df.append({'mutant' : mutant,
-                    'mean fraction looped' : mean_fraction,
-                    'std fraction looped' : std_fraction},
+                    'mean_loops_per_bead' : mean_fraction,
+                    'std_loops_per_bead' : std_fraction,
+                    'global_loops_per_bead' : global_fraction},
                     ignore_index=True)
-compiled_loop_df = compiled_loop_df[['mutant', 'mean fraction looped', 'std fraction looped']]
+compiled_loop_df = compiled_loop_df[['mutant', 'global_loops_per_bead', 'mean_loops_per_bead', 'std_loops_per_bead']]
 # Save to CSV
 compiled_loop_df.to_csv('../../data/compiled_bead_looping_fraction.csv',
                     index=False)
+#%%
+loop_df = loop_df.drop(columns=['average_beads_per_loop'])
+loop_df.to_csv('../../data/compiled_looping_fraction.csv')
 #%%
