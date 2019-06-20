a56a572d4f59780c1f2e537acb8360aa93968156
shirokaw <shirokaw@caltech.edu>
2019-06-20 14:40:30 -0700

    Changing from cutting to unlooping dynamics model.

--- vdj/model.py
+++ vdj/model.py
@@ -9,7 +9,7 @@ import pandas as pd
 
 
 
-def cutting_rate_log_likelihood(tau, t_cut, t_unloop):
+def unlooping_rate_log_likelihood(tau, t_cut, t_unloop):
     if len(t_cut) == 0:
         n_cuts = 0
         t_cut = np.array([0])
@@ -20,15 +20,15 @@ def cutting_rate_log_likelihood(tau, t_cut, t_unloop):
         t_unloop = np.array([0])
     else:
         n_unloop = len(t_unloop)
-    return -n_cuts * np.log(tau) - (n_cuts * t_cut.mean() + n_unloop * t_unloop.mean())/tau
+    return -n_unloop * np.log(tau) - (n_cuts * t_cut.mean() + n_unloop * t_unloop.mean())/tau
 
-def cutting_rate_log_prior(tau, alpha=0.86, beta=9.03):
+def unlooping_rate_log_prior(tau, alpha=0.86, beta=9.03):
     return st.invgamma(alpha, scale=beta).logpdf(tau)
     # return st.norm(120, 2).logpdf(tau)
 
-def cutting_rate_log_posterior(tau, t_cut, t_unloop, **kwargs):
-    log_like = cutting_rate_log_likelihood(tau, t_cut, t_unloop)
-    log_prior = cutting_rate_log_prior(tau, **kwargs)
+def unlooping_rate_log_posterior(tau, t_cut, t_unloop, **kwargs):
+    log_like = unlooping_rate_log_likelihood(tau, t_cut, t_unloop)
+    log_prior = unlooping_rate_log_prior(tau, **kwargs)
     return log_like  
 
 
