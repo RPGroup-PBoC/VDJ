37909687a1022f8cbc15316a4324265b3ffa7706
shirokaw <shirokaw@caltech.edu>
2019-06-20 13:56:25 -0700

    Fix legend.

--- code/figures/hmgb1_titration_dwells.py
+++ code/figures/hmgb1_titration_dwells.py
@@ -44,11 +44,11 @@ bootstrap_160_75 = np.percentile(x_160_sample, 75, axis=0)
 plt.figure(dpi=100)
 plt.fill_betweenx(y_40_sample, bootstrap_40_25, bootstrap_40_75, 
                 step='pre', facecolor='#429447',
-                alpha=0.65, label='40 nM bootstrap')
+                alpha=0.65, label='40 nM bootstrap, %i loops' %len(dwell_40))
 plt.fill_betweenx(y_160_sample, bootstrap_160_25, bootstrap_160_75,
                 step='pre', facecolor='#D43124',
-                alpha=0.65, label='160 nM bootstrap')
-plt.step(x_80, y_80, label='80 nM', color='#753F98')
+                alpha=0.65, label='160 nM bootstrap, %i loops' %len(dwell_160))
+plt.step(x_80, y_80, label='80 nM, %i loops' %len(dwell_80), color='#753F98')
 plt.xlabel('time (min)')
 plt.ylabel('bootstrapped ECDFs')
 plt.legend(loc='lower right')
@@ -56,9 +56,9 @@ plt.title('HMGB1 Titration (Bootstrapping)')
 plt.savefig('hmgb1_titration_bootstrap.pdf', facecolor='w')
 #%%
 plt.figure(dpi=100)
-plt.step(x_40, y_40, label='40 nM', color='#429447')
-plt.step(x_80, y_80, label='80 nM', color='#753F98')
-plt.step(x_160, y_160, label='160 nM', color='#D43124')
+plt.step(x_40, y_40, label='40 nM, %i loops' %len(dwell_40), color='#429447')
+plt.step(x_80, y_80, label='80 nM, %i loops' %len(dwell_80), color='#753F98')
+plt.step(x_160, y_160, label='160 nM, %i loops' %len(dwell_160), color='#D43124')
 plt.xlabel('time (min)')
 plt.ylabel('empirical CDF')
 plt.title('HMGB1 Titration (Calcium)')
