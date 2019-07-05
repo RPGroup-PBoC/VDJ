e333a8d318cd2178dee5c2465fa0f76db1173fc4
shirokaw <shirokaw@caltech.edu>
2019-07-05 14:56:09 -0700

    Update to loops/bead layout.

--- code/figures/loops_per_bead.py
+++ code/figures/loops_per_bead.py
@@ -3,6 +3,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.lines as lines
 import vdj.io 
 import vdj.viz
 vdj.viz.plotting_style()
@@ -49,28 +50,35 @@ points = counts[counts['n_muts'] == 1].copy()
 bar_width = 0.75
 fig, ax = plt.subplots(1, 1, figsize=(7, 4))
 
-colors = {'A':'tomato', 'T':'dodgerblue', 'C':'grey', 'G':'purple'}
-shift = {'A':-0.2, 'T':0.1, 'C':-0.1, 'G':0.2}
-ax.add_patch(plt.Rectangle((7.5,-0.35), 12.0, 0.04, facecolor='gray',
-            clip_on=False, alpha=0.4, linewidth=0))
+colors = {'A':'#E10C00', 'T':'#38C2F2', 'C':'#36C200', 'G':'#5919FF'}
+shift = {'A':-0.26, 'T':0.26, 'C':-0.13, 'G':0.13}
+
 for g, d in points.groupby(['base']):
     ax.plot(d['pos'] + shift[g] + 1, d['rel_diff'], marker='o', color=colors[g], lw=1, 
-                ms=3, linestyle='none', label=g)
-    ax.vlines(d['pos'] + shift[g] + 1, 0, d['rel_diff'], color=colors[g], lw=1, label='__nolegend__')
+                ms=4.5, linestyle='none', label=g)
+    ax.vlines(d['pos'] + shift[g] + 1, 0, d['rel_diff'], color=colors[g], lw=2, label='__nolegend__')
 
 _ = ax.set_xticks(np.arange(1, 29))
 _ = ax.set_xticklabels(list(ref_seq))
 ax.set_xlim([0.5, 28.5])
 
+line1 = lines.Line2D([7.5, 7.5], [-0.35, 0.-0.31], clip_on=False, alpha=1,
+                    linewidth=1, color='k')
+line2 = lines.Line2D([19.5, 19.5], [-0.35, 0.-0.31], clip_on=False, alpha=1,
+                    linewidth=1, color='k')
+ax.add_line(line1)
+ax.add_line(line2)
+
+
 ax.legend(fontsize=8, ncol=5)
-ax.set_xlabel('reference sequence')
+ax.set_xlabel('reference sequence', fontsize=12)
 ax.hlines(0, 0, 29, color='k', linestyle=':')
 ax.set_ylim([-0.3, 0.3])
-ax.set_ylabel('change in\nloop frequency')
+ax.set_ylabel('change in\nloop frequency', fontsize=12)
 for i in range(1, 29, 2):
-    ax.vlines(i, -0.4, 0.8, color='w', linewidth=12, zorder=-1)
-
-plt.savefig('./loop_frequency_stickplot_highlight_spacer.pdf', facecolor='white')
+    ax.axvspan(i-0.5, i+0.5, color='w', linewidth=0, zorder=-1)
+ax.vlines(0.5, -0.4, 0.4, color='#f5e3b3', linewidth=4, zorder=0)
+plt.savefig('./loop_frequency_stickplot.pdf', facecolor='white')
 #%%
 
 
