7d1db590925f25053027875fad15ff2e8cff3b25
Griffin Chure <gchure@caltech.edu>
2019-09-08 12:11:42 -0700

    Initial commit of endogenous details

--- code/figures/js/endogenous_explorer.js
+++ code/figures/js/endogenous_explorer.js
@@ -1,12 +1,21 @@
 // Define useful variables
-var mut = drop.value;
-filter.group = mut
+var mut = sel.value;
+filter.group = mut;
+
 
 // Set filters on view data
-seq_view.filters[0] = filter 
-
+seq_view.filters[0] = filter;
+dwell_view.filters[0] = filter;
+cut_view.filters[0] = filter;
+post_view.filters[0] = filter;
 // Update displayed view
-seqs.data.view = seq_view
+seqs.data.view = seq_view;
+dwell_hist.data.view = dwell_view;
+cut_hist.data.view = cut_view;
+post.data.view = post_view;
 
 // Push change
-seqs.change.emit()
+seqs.change.emit();
+dwell_hist.change.emit();
+cut_hist.change.emit();
+post.change.emit()
--- code/figures/js/endogenous_explorer.py
+++ code/figures/js/endogenous_explorer.py
@@ -12,18 +12,21 @@ from bokeh.models.widgets import Select
 from bokeh.embed import components
 import vdj.io
 import vdj.stats
-bokeh.io.output_notebook()  
+bokeh.plotting.output_file('./endogenous_voyager.html')
+
 
 #%%
+# ##############################################################################
+# LOAD DATA SETS AND RESTRICT TO ENDOGENOUS
+# ##############################################################################
 # Load the necessary data sets
 dwell_times = pd.read_csv('../../../data/compiled_dwell_times.csv')
-# posts = pd.read_csv('../../../data/coutting_rate_posteriors')
-# fates = pd.read_csv('../../../data/compiled_cutting_events.csv')
-
+posteriors = pd.read_csv('../../../data/pooled_cutting_probability_posteriors.csv')
+loops = pd.read_csv('../../../data/compiled_looping_events.csv')
 
 # Identify the endogenous muts
 dfs  = []
-for i, df in enumerate([dwell_times]):
+for i, df in enumerate([dwell_times, posteriors, loops]):
     for g, d in df.groupby(['mutant']):
         if ('12Spac' not in g) & ('12Hept' not in g) & ('12Non' not in g):
             mut_class = 'endogenous'
@@ -33,9 +36,50 @@ for i, df in enumerate([dwell_times]):
         df.loc[df['mutant']==g, 'class'] = mut_class
     df = df[(df['class']=='endogenous') & (df['salt']=='Mg') & (df['hmgb1']==80)]
     dfs.append(df)
-dwell_times = dfs[0]
+dwell_times, posteriors, loops = dfs
+cut_dwells = dwell_times[dwell_times['cut']==1]
 
+
+# %%
+# ##############################################################################
+# COMPUTE POOLED v REPLICATE LOOP FREQUENCIES
+# ##############################################################################
+pooled_df = pd.DataFrame()
+rep_df = pd.DataFrame()
+for g, d in loops.groupby('mutant'):
+    pooled_df = pooled_df.append({'mutant': g, 
+        'loops_per_bead':d['n_loops'].sum() / len(d['bead_idx'])},
+        ignore_index=True)
+    for _g, _d in d.groupby(['replicate']):
+        rep_df = rep_df.append({'mutant':g, 
+            'loops_per_bead':d['n_loops'].sum() / len(d['bead_idx'])},
+            ignore_index=True)
 #%%
+# ##############################################################################
+# GENERATE HISTOGRAMS OF DWELL TIMES 
+# ##############################################################################
+# Generate the histogrammed dwell times.
+bins = np.linspace(0, dwell_times['dwell_time_min'].max(), 25)
+dfs = []
+for source in [dwell_times, cut_dwells]: 
+    bin_dfs = []
+    for g, d in source.groupby('mutant'):
+        hist, bins = np.histogram(d['dwell_time_min'], bins=bins)
+        _df = pd.DataFrame()
+        _df['top'] = hist
+        _df['bottom'] = 0
+        _df['left'] = bins[1:]
+        _df['right'] = bins[:-1]
+        _df['mutant'] = g
+        bin_dfs.append(_df)
+    dwell_hist = pd.concat(bin_dfs)
+    dfs.append(dwell_hist)
+dwell_hist, cut_hist = dfs
+
+# %%
+# ##############################################################################
+#  DEFINE INVARIANT / VARIANT SEQUENCE MARKER
+# ##############################################################################
 # Find the invariable positions in the sequence
 ind = np.zeros((28, 5))
 seq = vdj.io.endogenous_seqs()
@@ -66,54 +110,135 @@ for g, d in df.groupby(['mutant']):
 
 variant = ColumnDataSource(var_dict)
 
+#%%
+# ##############################################################################
+# SOURCE AND VIEW DEFINITION
+# ##############################################################################
+# Set up the dropdown with the mutations
+mut_sel = Select(value='', options=list(dwell_times['mutant'].unique()))
 
+# Define the filter on the mutant props.
+mut_filter = GroupFilter(column_name="mutant", group=mut_sel.value)
 
-# %% 
+# Define the sources
+dwell_source = ColumnDataSource(dwell_hist)
+cut_source = ColumnDataSource(cut_hist)
+post_source = ColumnDataSource(posteriors)
+pooled_loop_source = ColumnDataSource(pooled_df)
+rep_loop_source = ColumnDataSource(rep_df)
+
+# Define the Views
+dwell_view = CDSView(source=dwell_source, filters=[mut_filter])
+cut_view = CDSView(source=cut_source, filters=[mut_filter])
+pooled_loop_view = CDSView(source=pooled_loop_source, filters=[mut_filter])
+rep_loop_view = CDSView(source=rep_loop_source, filters=[mut_filter])
+seq_view = CDSView(source=variant, filters=[mut_filter])
+post_view = CDSView(source=post_source, filters=[mut_filter])
+
+#%% 
+# ##############################################################################
+# DEFINE THE CANVASES
+# ##############################################################################
+ax_seq = bokeh.plotting.figure(height=40, y_range=[0, 0.1])
+ax_loop = bokeh.plotting.figure(height=100, 
+                                x_axis_label='paired complexes per bead')
+ax_dwell = bokeh.plotting.figure(height=200, x_axis_label='paired complex dwell time [min]',
+                                y_axis_label='number of observations')
+ax_cut = bokeh.plotting.figure(height=200, x_axis_label='cutting probability',
+                               y_axis_label='posterior probability')
+
+# Set features of the plots
+ax_seq.xaxis.visible = False
+ax_seq.yaxis.visible = False
+ax_seq.grid.visible = False
+
+# Define the layout
+lay = bokeh.layouts.column(mut_sel, ax_seq, ax_loop, ax_dwell, ax_cut) 
+
+# ##############################################################################
+# POPULATE CANVASES
+# ############################################################################## 
+# Sequence
+invar_glyph = bokeh.models.glyphs.Text(x='pos', y='y', text='base', text_font='Courier',
+                                    text_color='#95a3b2', text_font_size='28px')
+var_glyph = bokeh.models.glyphs.Text(x='pos', y='y', text='base', text_font='Courier',
+                                    text_color='skyblue', text_font_size='28px')
+ax_seq.add_glyph(invariant, invar_glyph)
+ax_seq.add_glyph(variant, var_glyph, view=seq_view)
+
+# Loops per bead
+ax_loop.circle(x='loops_per_bead', y=0, source=rep_loop_source,
+                view=rep_loop_view, color='slategray', legend='replicate')
+ax_loop.circle(x='loops_per_bead', y=0, source=pooled_loop_source,
+                view=pooled_loop_view, color='dodgerblue', legend='pooled')
+
+# Add the histogram
+ax_dwell.quad(left='left', bottom='bottom', top='top', right='right', 
+               view=dwell_view, source=dwell_source, color='dodgerblue',
+               alpha=0.5, legend="all PC events")
+ax_dwell.quad(left='left', bottom='bottom', top='top', right='right', 
+               view=cut_view, source=cut_source, color='slategrey',
+               alpha=0.5, hatch_pattern='/', legend="cleavage events")
+
+# Cutting probability posterior
+ax_cut.line(x='probability', y='posterior', source=post_source, view=post_view, 
+            color='dodgerblue')
+ax_cut.varea(x='probability', y1=0, y2='posterior', fill_color='dodgerblue',
+            fill_alpha=0.5, source=post_source, view=post_view)
+#
 # ##############################################################################
 # CALLBACK DEFINITION
 # ##############################################################################
 with open('endogenous_explorer.js') as f:
     cb = f.read()
 
-#%%
-# Set up the dropdown with the mutations
-mut_sel = Dropdown(label = "Select Endogenous Mutation", 
-            menu=[(m, m) for m in dwell_times['mutant'].unique()])
-
-# Define a global mutant filter.
-filter = GroupFilter(column_name="mutant", group="WT12rss")
-view = CDSView(source=seq_variant, filters=[filter])
-
-# Set up sequence axis
-ax_seq = bokeh.plotting.figure(height=40, y_range=[0, 0.1])
-ax_seq.xaxis.visible = False
-ax_seq.yaxis.visible = False
-ax_seq.grid.visible = False
-
-
-seq_glyph = bokeh.models.glyphs.Text(x='pos', y='y', text='base', text_font='Courier',
-
-                                    text_color='#95a3b2', text_font_size='28px')
-ax_seq.add_glyph(invariant, seq_glyph)
-var_glyph = bokeh.models.glyphs.Text(x='pos', y='y', text='base', text_font='Courier',
-                                    text_color='tomato', text_font_size='28px')
-ax_seq.add_glyph(seq_view, var_glyph)
-
-
-ax_loop = bokeh.plotting.figure(height=100)
-ax_dwell = bokeh.plotting.figure(height=250)
-ax_cut = bokeh.plotting.figure(height=250)
 # Set up the callback
-callback = CustomJS(args={'drop':mut_sel, 'view':view, 'seqs':variant,
-                          'filter':filter})
+callback = CustomJS(code=cb, args={'sel':mut_sel, 'seq_view':seq_view, 'seqs':variant,
+                          'filter':mut_filter, 'dwell_view':dwell_view, 'cut_view':cut_view,
+                          'cut_hist':cut_source,
+                          'dwell_hist':dwell_source, 'post_view':post_view,
+                          'post':post_source})
 mut_sel.js_on_change('value', callback)
 
 
-lay = bokeh.layouts.column(mut_sel, ax_seq) #, ax_loop, ax_dwell, ax_cut)
-bokeh.io.show(lay)
+#%% 
+# ##############################################################################
+# THEMING AND FILE I/O
+# ##############################################################################
+theme_json = {'attrs':
+            {'Figure': {
+                'background_fill_color': '#f5e3b3',
+                'outline_line_color': '#000000',
+            },
+            'Axis': {
+            'axis_line_color': "black",
+            'major_tick_out': 7,
+            'major_tick_line_width': 0.75,
+            'major_tick_line_color': "black",
+            'minor_tick_line_color': "black",
+            'axis_label_text_font': 'Helvetica',
+            'axis_label_text_font_style': 'normal'
+            },
+            'Grid': {
+                'grid_line_color': None,
+            },
+            'Legend': {
+                'background_fill_color': '#f5e3b3',
+                'border_line_color': '#FFFFFF',
+                'border_line_width': 1.5,
+            },
+            'Text': {
+                'text_font_style': 'normal',
+               'text_font': 'Helvetica'
+            },
+            'Title': {
+                'text_font_style': 'normal',
+                'align': 'center',
+                'text_font': 'Helvetica',
+                'offset': 2,
+            }}}
 
-#%%
+theme = Theme(json=theme_json)
+bokeh.io.curdoc().theme = theme
+bokeh.io.save(lay)
 
-
-
-#%%
