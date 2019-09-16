db4832334184239f5e0363cf218f2d2d307925d9
shirokaw <shirokaw@caltech.edu>
2019-09-16 19:34:48 -0400

    Faster bootstrapping.

--- code/analysis/looping_frequency_bootstrap.py
+++ code/analysis/looping_frequency_bootstrap.py
@@ -16,10 +16,9 @@ mutant_count=0
 for g,d in data.groupby(['mutant', 'salt', 'hmgb1']):
     mutant_count += 1
     print(mutant_count)
-    sampling = np.zeros(1000000)
-    for n in range(0, 1000000, 1):
-        sampling[n] = np.sum(d['n_loops'].sample(n=len(d),replace=True)) / len(d)
-    bs_low, bs_high = vdj.stats.compute_hpd(sampling, mass_frac=0.95)
+    sampling = np.random.choice(d['n_loops'].values,size=(len(d),10**6),replace=True)
+    loop_freq = np.sum(sampling, axis=1) / len(d)
+    bs_low, bs_high = vdj.stats.compute_hpd(loop_freq, mass_frac=0.95)
     mean = np.mean(sampling)
     df = pd.DataFrame({'mean':[mean], 
                         'bs_low':[bs_low], 
@@ -27,6 +26,7 @@ for g,d in data.groupby(['mutant', 'salt', 'hmgb1']):
     df['mutant'] = g[0]
     df['salt'] = g[1]
     df['hmgb1'] = g[2]
-    bs_df = bs_df.append(df)
-
+    bs_df = bs_df.append(df, ignore_index=True)
+bs_df = bs_df[['mutant', 'salt', 'hmgb1', 'mean', 'bs_low', 'bs_high']]
 #%%
+bs_df.to_csv('../../data/compiled_loop_freq_bs.csv')
