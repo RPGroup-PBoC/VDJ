30822783297877a0511875c9a135bfbb17a2f3f5
Griffin Chure <gchure@caltech.edu>
2019-09-17 13:53:52 -0700

    Added reworked bootstrapping

--- code/analysis/looping_frequency_bootstrap.py
+++ code/analysis/looping_frequency_bootstrap.py
@@ -3,32 +3,31 @@ import numpy as np
 import pandas as pd
 import vdj.io
 import vdj.stats
-from tqdm import tqdm
+import tqdm
 
 #%%
-data = pd.read_csv('../../data/compiled_looping_events.csv', sep=',')
+data = pd.read_csv('../../data/compiled_looping_events.csv')
 
 #%%
-# Need to group by mutant, Ca and HMGB1 concentration
-bs_df = pd.DataFrame(columns=['mean','bs_low','bs_high',
-                            'mutant','salt','hmgb1'])
-mutant_count=0
-for g,d in data.groupby(['mutant', 'salt', 'hmgb1']):
-    mutant_count += 1
-    print(mutant_count)
-    sampling = np.random.choice(d['n_loops'].values,size=(len(d),10**6),replace=True)
+percentiles = [(2.5, 97.5), (87.5, 12.5), (25, 75), (37.5, 62.5), (45, 55), 
+               (47.5, 52.5)]
+col_names = [("bs_95_low", "bs_95_high"), ("bs_75_low", "bs_75_high"),
+             ("bs_50_low", "bs_50_high"),  ("bs_25_low", "bs_25_high"),
+             ("bs_10_low", "bs_10_high"), ("bs_5_low", "bs_5_high")]
+bs_reps = int(1E6)
+bs_df = pd.DataFrame([])
+for g,d in tqdm.tqdm(data.groupby(['mutant', 'salt', 'hmgb1'])):
+    sampling = np.random.choice(d['n_loops'].values,size=(len(d), bs_reps),replace=True)
     loop_freq = np.sum(sampling, axis=0) / len(d)
-    bs_low, bs_high = vdj.stats.compute_hpd(loop_freq, mass_frac=0.95)
-    mean = np.mean(loop_freq)
-    df = pd.DataFrame({'mean':[mean], 
-                        'bs_low':[bs_low], 
-                        'bs_high':[bs_high]})
-    df['mutant'] = g[0]
-    df['salt'] = g[1]
-    df['hmgb1'] = g[2]
-    bs_df = bs_df.append(df, ignore_index=True)
-bs_df = bs_df[['mutant', 'salt', 'hmgb1', 'mean', 'bs_low', 'bs_high']]
-#%%
+    df_dict = {'mutant':g[0], 'salt':g[1], 'hmgb1':g[2],
+                'n_loops':d['n_loops'].sum(), 'n_beads':len(d),
+                'loops_per_bead':d['n_loops'].sum() / len(d)}
+    for perc, col in zip(percentiles, col_names):
+        computed_percentiles = np.percentile(loop_freq, perc)
+        for i in range(2):
+            df_dict[col[i]] = computed_percentiles[i]
+
+    bs_df = bs_df.append(df_dict, ignore_index=True)
 bs_df.to_csv('../../data/compiled_loop_freq_bs.csv')
 
 #%%
