d0e2d64507b2870113b674bf26e5c02a5fee8f79
Griffin Chure <gchure@caltech.edu>
2019-09-18 14:11:10 -0700

    Updated things on website

---
+++ _site/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+from os import path
+
+__version__ = '0.0.01'
+
+setup(
+    name='vdj',
+    version=__version__,
+    description='Python utilities used for analysis of vdj data.',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 0 - mega-alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3',
+    ],
+    author="Soichi Hirokawa and Griffin Chure",
+)
