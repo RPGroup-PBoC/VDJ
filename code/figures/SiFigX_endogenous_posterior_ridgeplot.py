35ff0c825f1eef7c25292abdce39fd15016cb18c
shirokaw <shirokaw@caltech.edu>
2019-09-19 15:28:59 -0400

    Fixing facecolor of endogenous ridgeplot.

--- code/figures/SiFigX_endogenous_posterior_ridgeplot.py
+++ code/figures/SiFigX_endogenous_posterior_ridgeplot.py
@@ -61,6 +61,7 @@ ax.hlines(offset_dict["DFL16.1-3'"] + 0.025, 0.7, 0.73, lw=2)
 ax.text(0.74, offset_dict["DFL16.1-3'"] + 0.012, '$\propto$probability', fontsize=12)
 ax.set_xlim([0.0, 1.0])
 ax.set_yticklabels([])
+ax.set_facecolor('white')
 ax.set_xlabel('$p_{cut}$', fontsize=14)
 plt.savefig('./SiFigX_endogenous_posterior_ridgeplot.pdf',
             facecolor='white', bbox_inches='tight')
