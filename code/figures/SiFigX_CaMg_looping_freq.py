858a216d1f3f0d0dccf69c34ccd0d43c95bfd362
shirokaw <shirokaw@caltech.edu>
2019-09-19 15:05:17 -0400

    SI Fig on calcium-magnesium looping frequency.

--- code/figures/SiFigX_CaMg_looping_freq.py
+++ code/figures/SiFigX_CaMg_looping_freq.py
@@ -28,7 +28,7 @@ for mut in muts:
     ca_loops = df_loops[(df_loops['mutant']==mut) & (df_loops['salt']=='Ca')]['loops_per_bead'].values[0]
     mg_loops = df_loops[(df_loops['mutant']==mut) & (df_loops['salt']=='Mg')]['loops_per_bead'].values[0]
     ax[muts[mut]].set_xlim([0, 1.0])
-    ax[muts[mut]].set_ylim([0, 0.1])
+    ax[muts[mut]].set_ylim([0.01, 0.08])
     ax[muts[mut]].set_yticklabels([])
 
     if muts[mut]!=2:
@@ -48,21 +48,26 @@ for mut in muts:
                                     (d['high'].values[0] - d['low'].values[0]),
                                     0.024, color='rebeccapurple', alpha=0.15)
             ax[muts[mut]].add_patch(rect)
-    ax[muts[mut]].text(0.98, 0.085, mut, fontsize=12, ha='right')
+    ax[muts[mut]].text(0.98, 0.015, mut, fontsize=14, ha='right')
 ax[2].set_xlabel('loop frequency', fontsize=14)
 
 # Draw rectangles as legend
 confidence_intervals = np.sort(camg_data['percentile'].unique())
-widths = np.linspace(0, 0.02 * (len(confidence_intervals) - 1), len(confidence_intervals))
-ci_dict = {str(ci):w for ci,w in zip(confidence_intervals, widths)}
+widths = np.linspace(0.05, 0.06 * (len(confidence_intervals)), len(confidence_intervals))
+ci_dict = {str(int(ci)):w for ci,w in zip(confidence_intervals, widths)}
 for ci in ci_dict:
-    ci_rect_ca = patches.Rectangle((0.2, 0.105), ci_dict[ci], 0.024, 
+    ci_rect_ca = patches.Rectangle((0.10, 0.095), ci_dict[ci], 0.024, 
                                     color='green', alpha=0.15, clip_on=False)
-    ci_rect_mg = patches.Rectangle((0.65, 0.105), ci_dict[ci], 0.024, 
+    ci_rect_mg = patches.Rectangle((0.60, 0.095), ci_dict[ci], 0.024, 
                                     color='rebeccapurple', alpha=0.15, clip_on=False)
     ax[0].add_patch(ci_rect_ca)
     ax[0].add_patch(ci_rect_mg)
-
+    ax[0].text(0.08 + ci_dict[ci], 0.085, ci + '%', ha='center')
+    ax[0].text(0.58 + ci_dict[ci], 0.085, ci + '%', ha='center')
+ax[0].text(0.25, 0.125, r'Ca$^{2+}$', fontsize=14)
+ax[0].text(0.75, 0.125, r'Mg$^{2+}$', fontsize=14)
+plt.tight_layout()
+plt.savefig('./SiFigX_CaMg_looping_freq.pdf', facecolor='white', bbox_inches='tight')
 #%%
 
 
