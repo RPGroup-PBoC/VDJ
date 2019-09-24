272ec4e36d38d25275bc3f6019e1ab1e236fb404
shirokaw <shirokaw@caltech.edu>
2019-09-24 15:39:15 -0400

    Thought I saved .py file.

--- code/figures/SiFigX_coding_flanks.py
+++ code/figures/SiFigX_coding_flanks.py
@@ -52,45 +52,46 @@ ax.set_facecolor('white')
 gs_ci = gridspec.GridSpec(1, 3)
 ax_conf_int = fig.add_subplot(gs_ci[:, :])
 gs_ci.tight_layout(fig, rect=[0, 1.5, 1.5, 1.75])
-ax_conf_int.set_xlim([0, 1.0])
+ax_conf_int.set_xlim([0.15, 0.78])
 ax_conf_int.set_xticklabels([])
 ax_conf_int.set_yticklabels([])
 ax_conf_int.set_facecolor('white')
 
-gs = gridspec.GridSpec(1, 3, hspace=0.2, wspace=0.4)
+gs = gridspec.GridSpec(1, 3, hspace=0.2, wspace=0.6)
 ax_loop = fig.add_subplot(gs[0])
 ax_loop.set_xticklabels([])
 ax_loop.set_xlim([0.125, 0.625])
 ax_loop.set_ylim([0, 0.3])
 ax_loop.set_yticklabels([0, '', 0.1, '', 0.2, '', 0.3])
-ax_loop.set_ylabel('looping frequency', fontsize=12)
+ax_loop.set_ylabel('looping frequency', fontsize=16)
 
 ax_posts = fig.add_subplot(gs[1:])
-ax_posts.set_xlabel('cutting probability', fontsize=12)
-ax_posts.set_ylabel('posterior distribution', fontsize=12)
+ax_posts.set_xlabel('cutting probability', fontsize=16)
+ax_posts.set_ylabel('posterior distribution', fontsize=16)
 
 gs.tight_layout(fig, rect=[0, 0.75, 1.5, 1.55])
 
 gs2 = gridspec.GridSpec(1, 3)
 ax_dwell_cut = fig.add_subplot(gs2[0])
 ax_dwell_cut.set_xscale('log')
-ax_dwell_cut.set_xlabel('dwell time [min]', fontsize=12)
-ax_dwell_cut.set_ylabel('ECDF', fontsize=12)
+ax_dwell_cut.set_xlabel('dwell time [min]', fontsize=16)
+ax_dwell_cut.set_ylabel('ECDF', fontsize=16)
 
 ax_dwell_unloop = fig.add_subplot(gs2[1])
 ax_dwell_unloop.set_xscale('log')
-ax_dwell_unloop.set_xlabel('dwell time [min]', fontsize=12)
+ax_dwell_unloop.set_xlabel('dwell time [min]', fontsize=16)
 ax_dwell_unloop.set_yticklabels([])
 
 ax_dwell_all = fig.add_subplot(gs2[2])
 ax_dwell_all.set_xscale('log')
-ax_dwell_all.set_xlabel('dwell time [min]', fontsize=12)
+ax_dwell_all.set_xlabel('dwell time [min]', fontsize=16)
 ax_dwell_all.set_yticklabels([])
 
 gs2.tight_layout(fig, rect=[0, 0, 1.5, 0.8])
 
 seqs = {'V4-57-1 (ref)':0.25, '12CodC6A':0.5}
 colors = {'V4-57-1 (ref)':'slategrey', '12CodC6A':'dodgerblue'}
+perc_widths = {p:w for p, w in zip(np.sort(loop['percentile'].unique()), np.arange(0.03, 0.21, 0.03))}
 
 for seq in seqs:
     ax_loop.scatter(seqs[seq], loop[loop['mutant']==seq]['loops_per_bead'].values[0], 
@@ -100,7 +101,11 @@ for seq in seqs:
                                 d['high'].values[0] - d['low'].values[0],
                                 color=colors[seq], alpha=0.2)
         ax_loop.add_patch(rect)
-        rect2 = patches.Rectangle([])
+        rect2 = patches.Rectangle([seqs[seq], 0.1], perc_widths[g], 0.5, alpha=0.2, color=colors[seq])
+        ax_conf_int.add_patch(rect2)
+        ax_conf_int.text(seqs[seq] + perc_widths[g] - 0.015, 0.05, str(int(g)) + '%',
+                        ha='center', va='top')
+
     ax_dwell_cut.plot(cut_dist[cut_dist['mutant']==seq]['x'], 
                         cut_dist[cut_dist['mutant']==seq]['y'],
                         lw=2, color=colors[seq])
@@ -116,76 +121,92 @@ for seq in seqs:
     ax_posts.fill_between(cut_posts[cut_posts['mutant']==seq]['probability'],
                             0, cut_posts[cut_posts['mutant']==seq]['posterior'],
                             color=colors[seq], alpha=0.4)
-
+    
+    ax_conf_int.text(seqs[seq] + 0.09, 0.65, seq, ha='center', va='bottom', fontsize=12)
+plt.savefig('./SiFigX_cod_ref.pdf', bbox_inches='tight', facecolor='white')
 #%%
-ax_conf_int = bokeh.plotting.figure(height=50, width=570, 
-                                    x_range=[0.125, 0.805], y_range=[0, 0.1])
-ax_conf_int.xaxis.visible = False
-ax_conf_int.yaxis.visible = False
-ax_conf_int.background_fill_color = 'white'
-
-ax_loop = bokeh.plotting.figure(height=200, width=210, 
-                                x_range=[0.1,0.65], y_range=[0.0,0.35],
-                                y_axis_label='looping frequency')
-ax_loop.xaxis.visible=False
-ax_loop.yaxis.ticker=[0, 0.1, 0.2, 0.3]
-
-ax_dwell_cut = bokeh.plotting.figure(height=190, width=210, x_axis_type='log',
-                                    x_range=[0.5,80], y_axis_label='ECDFs',
-                                    x_axis_label='dwell time [min]')
-ax_dwell_unloop = bokeh.plotting.figure(height=190, width=180, x_axis_type='log',
-                                    x_range=[0.5, 80], x_axis_label='dwell time [min]')
-ax_dwell_all = bokeh.plotting.figure(height=190, width=180, x_axis_type='log',
-                                    x_range=[0.5, 80], x_axis_label='dwell time [min]')
-
-ax_posts = bokeh.plotting.figure(height=200, width=360, 
-                                x_range=[0, 1.0], x_axis_label='cutting probability',
-                                y_axis_label='posterior')
-ax_posts.yaxis.ticker = [0, 0.01, 0.02]
-
 seqs = {'V4-55':0.25, '12SpacC1A':0.5}
 colors = {'V4-55':'slategrey', '12SpacC1A':'dodgerblue'}
 
-ci_widths = np.arange(0.03, 0.03 * (len(loop['percentile'].unique()) + 1), 0.03)
-ci_centers = np.arange(0.015, 0.015 * (len(loop['percentile'].unique()) + 1), 0.015)
-ci_text = np.arange(0.015, 0.015 + 0.030 * (len(loop['percentile'].unique())), 0.03)
-ci_y = 0.08
-ci_perc = np.sort(loop['percentile'].unique())
-ci_df = pd.DataFrame({'percentile':ci_perc, 'y':ci_y, 'text':ci_text, 
-                    'center':ci_centers, 'width':ci_widths})
-ci_df['perc_text'] = list(str(int(perc)) for perc in ci_perc)
-text_source = ColumnDataSource(ci_df)
+fig = plt.figure()
+ax = plt.gca()
+ax.get_xaxis().set_ticks([])
+ax.get_yaxis().set_ticks([])
+ax.set_facecolor('white')
+
+gs_ci = gridspec.GridSpec(1, 3)
+ax_conf_int = fig.add_subplot(gs_ci[:, :])
+gs_ci.tight_layout(fig, rect=[0, 1.5, 1.5, 1.75])
+ax_conf_int.set_xlim([0.15, 0.78])
+ax_conf_int.set_xticklabels([])
+ax_conf_int.set_yticklabels([])
+ax_conf_int.set_facecolor('white')
+
+gs = gridspec.GridSpec(1, 3, hspace=0.2, wspace=0.4)
+ax_loop = fig.add_subplot(gs[0])
+ax_loop.set_xticklabels([])
+ax_loop.set_xlim([0.125, 0.625])
+ax_loop.set_ylim([0, 0.3])
+ax_loop.set_yticklabels([0, '', 0.1, '', 0.2, '', 0.3])
+ax_loop.set_ylabel('looping frequency', fontsize=16)
+
+ax_posts = fig.add_subplot(gs[1:])
+ax_posts.set_xlabel('cutting probability', fontsize=16)
+ax_posts.set_ylabel('posterior distribution', fontsize=16)
+
+gs.tight_layout(fig, rect=[0, 0.75, 1.5, 1.55])
+
+gs2 = gridspec.GridSpec(1, 3)
+ax_dwell_cut = fig.add_subplot(gs2[0])
+ax_dwell_cut.set_xscale('log')
+ax_dwell_cut.set_xlabel('dwell time [min]', fontsize=16)
+ax_dwell_cut.set_ylabel('ECDF', fontsize=16)
+
+ax_dwell_unloop = fig.add_subplot(gs2[1])
+ax_dwell_unloop.set_xscale('log')
+ax_dwell_unloop.set_xlabel('dwell time [min]', fontsize=16)
+ax_dwell_unloop.set_yticklabels([])
+
+ax_dwell_all = fig.add_subplot(gs2[2])
+ax_dwell_all.set_xscale('log')
+ax_dwell_all.set_xlabel('dwell time [min]', fontsize=16)
+ax_dwell_all.set_yticklabels([])
+
+gs2.tight_layout(fig, rect=[0, 0, 1.5, 0.8])
+
+perc_widths = {p:w for p, w in zip(np.sort(loop['percentile'].unique()), np.arange(0.03, 0.21, 0.03))}
 
 for seq in seqs:
-    ax_loop.triangle(seqs[seq], loop[loop['mutant']==seq]['loops_per_bead'], 
-                    size=15, fill_color='white', line_color=colors[seq], level='overlay')
+    ax_loop.scatter(seqs[seq], loop[loop['mutant']==seq]['loops_per_bead'].values[0], 
+                    s=150, marker='^', color='white', edgecolor=colors[seq], zorder=10)
     for g,d in loop[loop['mutant']==seq].groupby('percentile'):
-        ax_loop.rect(seqs[seq], (d['high'] + d['low']) / 2, 0.15,
-                    d['high'] - d['low'], color=colors[seq], alpha=0.2)
-        ax_conf_int.rect(seqs[seq] + ci_df[ci_df['percentile']==g]['center'], 0.05,
-                        ci_df[ci_df['percentile']==g]['width'], 0.025, color=colors[seq],
-                        alpha=0.2)
-#        Text(x='ci_text', y='y', text='perc_text', source=text_source)
-    ax_dwell_cut.line(cut_dist[cut_dist['mutant']==seq]['x'], 
-                        cut_dist[cut_dist['mutant']==seq]['y'],
-                        line_width=2, line_color=colors[seq])
-    ax_dwell_unloop.line(unloop_dist[unloop_dist['mutant']==seq]['x'], 
-                        unloop_dist[unloop_dist['mutant']==seq]['y'],
-                        line_width=2, line_color=colors[seq])
-    ax_dwell_all.line(dwell_dist[dwell_dist['mutant']==seq]['x'], 
-                        dwell_dist[dwell_dist['mutant']==seq]['y'],
-                        line_width=2, line_color=colors[seq])
-    ax_posts.line(cut_posts[cut_posts['mutant']==seq]['probability'],
-                cut_posts[cut_posts['mutant']==seq]['posterior'],
-                line_width=2, line_color=colors[seq])
-    ax_posts.varea(cut_posts[cut_posts['mutant']==seq]['probability'],
-                0, cut_posts[cut_posts['mutant']==seq]['posterior'],
-                color=colors[seq], alpha=0.4)
+        rect = patches.Rectangle([seqs[seq] - 0.075, d['low']], 0.15,
+                                d['high'].values[0] - d['low'].values[0],
+                                color=colors[seq], alpha=0.2)
+        ax_loop.add_patch(rect)
+        rect2 = patches.Rectangle([seqs[seq], 0.1], perc_widths[g], 0.5, alpha=0.2, color=colors[seq])
+        ax_conf_int.add_patch(rect2)
+        ax_conf_int.text(seqs[seq] + perc_widths[g] - 0.015, 0.05, str(int(g)) + '%',
+                        ha='center', va='top')
 
-top_row = bokeh.layouts.row(ax_loop, ax_posts)
-dwell_row = bokeh.layouts.row(ax_dwell_cut, ax_dwell_unloop, ax_dwell_all)
-column = bokeh.layouts.column(ax_conf_int, top_row, dwell_row)
-bokeh.io.show(column)
+    ax_dwell_cut.plot(cut_dist[cut_dist['mutant']==seq]['x'], 
+                        cut_dist[cut_dist['mutant']==seq]['y'],
+                        lw=2, color=colors[seq])
+    ax_dwell_unloop.plot(unloop_dist[unloop_dist['mutant']==seq]['x'], 
+                        unloop_dist[unloop_dist['mutant']==seq]['y'],
+                        lw=2, color=colors[seq])
+    ax_dwell_all.plot(dwell_dist[dwell_dist['mutant']==seq]['x'], 
+                        dwell_dist[dwell_dist['mutant']==seq]['y'],
+                        lw=2, color=colors[seq])
+    ax_posts.plot(cut_posts[cut_posts['mutant']==seq]['probability'],
+                    cut_posts[cut_posts['mutant']==seq]['posterior'],
+                    lw=2, color=colors[seq])
+    ax_posts.fill_between(cut_posts[cut_posts['mutant']==seq]['probability'],
+                            0, cut_posts[cut_posts['mutant']==seq]['posterior'],
+                            color=colors[seq], alpha=0.4)
+    
+    ax_conf_int.text(seqs[seq] + 0.09, 0.65, seq, ha='center', va='bottom', fontsize=12)
+plt.savefig('./SiFigX_spac_endog.pdf', bbox_inches='tight', facecolor='white')
 
 #%%
 
