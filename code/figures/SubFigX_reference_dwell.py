8ccc95b6b6edc6b6f346e352f27216b49378231d
shirokaw <shirokaw@caltech.edu>
2020-01-27 11:10:06 -0800

    Updates to .py files.

---
+++ code/figures/SubFigX_reference_dwell.py
@@ -0,0 +1,39 @@
+import numpy as np
+import pandas as pd
+import vdj.io
+import vdj.viz
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+vdj.viz.plotting_style()
+
+# Load the dwell times
+dwell = pd.read_csv('../../data/compiled_dwell_times.csv', comment='#')
+dwell = dwell[(dwell['salt']=='Mg') & (dwell['hmgb1']==80) & (dwell['mutant']=='WT12rss')]
+
+#%%
+fig,ax = plt.subplots(1, 1, figsize=(9,3))
+bins=np.arange(0, 20, 1.0)
+ax.hist(dwell['dwell_time_min'], color='#e28371', bins=bins)
+ax.plot(dwell['dwell_time_min'].median(), 15, color='dodgerblue', lw=1.25,
+            ms=25, zorder=10, marker='o', markerfacecolor='white')
+ax.hlines(15, dwell['dwell_time_min'].quantile(0.25),
+        dwell['dwell_time_min'].quantile(0.75), color='dodgerblue',
+        lw=5, ls='-', zorder=10)
+ax.text(dwell['dwell_time_min'].median(), 14.6, 'N', fontsize=18, zorder=20,
+        color='dodgerblue', horizontalalignment='center',
+        verticalalignment='center')
+
+_ = ax.set_xticks(np.arange(0, 30, 5))
+_ = ax.set_xticklabels(np.arange(0, 30, 5), fontsize=18)
+_ = ax.set_xlim([0, 20])
+_ = ax.set_yticks(np.arange(0, 40, 10))
+_ = ax.set_yticklabels(np.arange(0, 40, 10), fontsize=18)
+_ = ax.set_ylabel('counts', fontsize=28)
+_ = ax.set_xlabel('time [min]', fontsize=28)
+_ = ax.tick_params(direction='out', length=0, width=2,
+                labelsize=20, right=False, top=False)
+
+fig.savefig('./figures/SubFigB_reference_dwell_histogram.pdf', bbox_inches='tight',
+            facecolor='white')
+
+# %%
